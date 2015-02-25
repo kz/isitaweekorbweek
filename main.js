3e0d890532446e242210df88b60f97dd5dc437d8
Kelvin Zhang <github@iamkelv.in>
2015-02-25 13:54:23 +0000

    Add countdown timer

--- main.js
+++ main.js
@@ -57,9 +57,15 @@ var nextDate = getEarliestDateFromNow(dates, now.format("YYYYMMDD"));
 var nextMoment = moment(nextDate, "YYYYMMDD");
 var nextWeek = dates[getKeyFromArray(dates, nextDate)][nextDate];
 if (nextWeek === "A") {
-    document.getElementById("next").innerHTML = "<b>" + nextWeek + " week</b> starts in <b>" + nextMoment.countdown(now, countdown.DAYS|countdown.HOURS).toString() + "</b>.";
+    document.getElementById("next").innerHTML = "<b>" + nextWeek + " week</b> starts in <b><span id=\"countdown-timer\"></span></b>.";
 } else if (nextWeek === "B") {
-    document.getElementById("next").innerHTML = "<b>" + nextWeek + " week</b> starts in <b>" + nextMoment.countdown(now, countdown.DAYS|countdown.HOURS).toString() + "</b>.";
+    document.getElementById("next").innerHTML = "<b>" + nextWeek + " week</b> starts in <b><span id=\"countdown-timer\"></span></b>.";
 } else {
     document.getElementById("next").innerHTML = "Error - Please email me at <a href=\"mailto:updatethissite@iamkelv.in\">updatethissite@iamkelv.in</a> and tell me to update this site!";
 }
+
+var timer = document.getElementById("countdown-timer");
+timer.innerHTML = nextMoment.countdown(now).toString();
+setInterval(function() {
+	timer.innerHTML = nextMoment.countdown(moment()).toString();
+}, 1000);
